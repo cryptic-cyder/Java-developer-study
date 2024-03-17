1558bd5d04a6520000ef914c2dd369492d898532
Shahriar Badhon <shahriarbadhon778@gmail.com>
2024-03-17 17:23:05 +0600

    Add code of memory fregmentation

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        try{
+            // Attempt to create large arrays until OutOfMemoryError occurs
+            for (int i = 1; i <= 1000000000; i++) {
 
+                int[] largeArray = new int[1000000]; // Each array has 1 million elements
+                System.out.println("Created array #" + i);
+            }
+        }
+        catch (OutOfMemoryError outOfMemoryError){
+            System.out.println(outOfMemoryError);
+        }
     }
 }
